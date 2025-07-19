64df6ca422b3e2570d5ad8768154b837a67c5960
fikriaf <143471086+fikriaf@users.noreply.github.com>
2025-07-19 09:03:51 +0700

    update

--- aivision-frontend/vite.config.ts
+++ aivision-frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // ← wajib untuk resolve path
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // alias @ ke ./src
+    },
+  },
 })
