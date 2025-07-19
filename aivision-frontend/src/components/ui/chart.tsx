84f01940ae3b2af87bdcbb6e77dbb43bcb3d7d52
fikriaf <143471086+fikriaf@users.noreply.github.com>
2025-07-19 10:11:14 +0700

    update

--- aivision-frontend/src/components/ui/chart.tsx
+++ aivision-frontend/src/components/ui/chart.tsx
@@ -2,6 +2,8 @@
 
 import * as React from "react"
 import * as RechartsPrimitive from "recharts"
+import type { LegendPayload } from "recharts/types/component/DefaultLegendContent"
+
 
 import { cn } from "@/lib/utils"
 
@@ -185,7 +187,7 @@ const ChartTooltipContent = React.forwardRef<
       >
         {!nestLabel ? tooltipLabel : null}
         <div className="grid gap-1.5">
-          {payload.map((item, index) => {
+          {payload.map((item: any, index: any) => {
             const key = `${nameKey || item.name || item.dataKey || "value"}`
             const itemConfig = getPayloadConfigFromPayload(config, item, key)
             const indicatorColor = color || item.payload.fill || item.color
